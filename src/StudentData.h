3a09f57c1befc9cfd82615147f5cecf3100cb8f7
JouderMin <joudermin@foxmail.com>
2021-05-05 19:48:02 +0800

    JouderMin: 增加基本结构体数据StudentData

---
+++ src/StudentData.h
@@ -0,0 +1,17 @@
+//
+// Created by 22176 on 2021/5/5.
+//
+
+#ifndef STUDENT_MANAGEMENT_SYSTEM_SSPU_STUDENTDATA_H
+#define STUDENT_MANAGEMENT_SYSTEM_SSPU_STUDENTDATA_H
+
+struct StudentData {
+    int number;
+    int classCode;
+    int result_course_1;
+    int result_course_2;
+    int result_course_3;
+    char name[20];
+};
+
+#endif // STUDENT_MANAGEMENT_SYSTEM_SSPU_STUDENTDATA_H
