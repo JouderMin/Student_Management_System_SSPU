7a81b3f14dfcab4756e17c53844e6dc62dcce678
祖安 <3060928633@qq.com>
2021-05-29 18:37:27 +0800

    钟勇：写了一点排序

---
+++ src/DataSort.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 祖安 on 2021/5/29.
+//
+
+#include "DataSort.h"
+#include "FileOperation.h"
+#include <algorithm>
+
+using std::sort;
+using std::vector;
+
+int sum(StudentData a) {
+    return a.result_course_1 + a.result_course_2 + a.result_course_3;
+}
+
+bool sort_by_sum_core(StudentData a, StudentData b) {
+    return sum(a) > sum(b);
+}
+
+vector<StudentData> sort_by_sum() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_sum_core);
+
+    return data;
+}
---
+++ src/DataSort.h
@@ -0,0 +1,15 @@
+//
+// Created by 祖安 on 2021/5/29.
+//
+
+#ifndef STUDENT_MANAGEMENT_SYSTEM_SSPU_DATASORT_H
+#define STUDENT_MANAGEMENT_SYSTEM_SSPU_DATASORT_H
+
+#include "StudentData.h"
+#include <vector>
+
+using std::vector;
+
+vector<StudentData> sort_by_sum();
+
+#endif // STUDENT_MANAGEMENT_SYSTEM_SSPU_DATASORT_H
