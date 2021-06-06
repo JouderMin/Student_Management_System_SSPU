8028914fae2162fa232a728debd774711c1cd5d2
祖安 <3060928633@qq.com>
2021-06-06 09:49:46 +0800

    完成了排序部分

--- src/DataSort.cpp
+++ src/DataSort.cpp
@@ -26,3 +26,68 @@ vector<StudentData> sort_by_sum() {
 
     return data;
 }
+
+bool sort_by_num_core(const StudentData & a, const StudentData & b) {
+    return a.number > b.number;
+}
+
+vector<StudentData> sort_by_num() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_num_core);
+    return data;
+}
+
+bool sort_by_class_core(const StudentData & a, const StudentData & b) {
+    return a.classCode > b.classCode;
+}
+
+vector<StudentData> sort_by_class() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_class_core);
+    return data;
+}
+
+bool sort_by_course_result_1_core(const StudentData & a, const StudentData & b) {
+    return a.result_course_1 > b.result_course_1;
+}
+
+vector<StudentData> sort_by_course_result_1() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_course_result_1_core);
+    return data;
+}
+
+bool sort_by_course_result_2_core(const StudentData & a, const StudentData & b) {
+    return a.result_course_2 > b.result_course_2;
+}
+
+vector<StudentData> sort_by_course_result_2() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_course_result_2_core);
+    return data;
+}
+
+bool sort_by_course_result_3_core(const StudentData & a, const StudentData & b) {
+    return a.result_course_3 > b.result_course_3;
+}
+
+vector<StudentData> sort_by_course_result_3() {
+    auto * file = new FileOperation;
+    vector<StudentData> data;
+    file->allLog(data);
+    delete file;
+    sort(data.begin(), data.begin() + (signed)data.size(), sort_by_course_result_3_core);
+    return data;
+}
